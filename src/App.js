4f1ddc27f024737d1648045f6107a25d36e27fa2
Alex Chan <Alex.Y.Chan.19@dartmouth.edu>
2018-11-12 05:29:18 -0500

    adding slaps

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import roofSlap from './roof_slap.jpg';
 import './App.css';
+import * as axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      slapText: '',
+    }
+  }
+
+  onSubmit = async () => {
+    let config = {
+      headers: {'Access-Control-Allow-Origin': '*'}
+    }
+    const res = await axios.get(`https://roof-slapping-bot.herokuapp.com/word/${this.state.text}`, config )
+
+    let newText; 
+    if (!!res.data.text) {
+      newText = res.data.text;
+    } else {
+      newText = 'Sorry no results. Input a new noun.'
+    }
+    this.setState({slapText: newText});
+
+  }
+
+  onChange = (e) => {
+    this.setState({[e.target.name]: e.target.value});
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h2>Roof Slapping Bot</h2>
+          <div className="searchDiv">
+            <input className="searchBar" onChange={this.onChange} type="text" name="text" autoComplete="off" placeholder="Type in a noun..."></input>
+            <button onClick={this.onSubmit} className="btn">search</button>
+          </div>
+          <p className="slapText">{this.state.slapText}</p>
+          <img src={roofSlap} className="App-logo" alt="logo" />
+          <p className="footerText">Inspired by <a href="https://twitter.com/roofslappingbot?lang=en">roof slapping twitter bot</a></p>
         </header>
       </div>
     );
